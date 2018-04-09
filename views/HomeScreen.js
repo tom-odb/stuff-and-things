b3828b24803741ac25dbf90826705b96bd784cfc
Tom Opdebeeck <tom.opdebeeck@district01.be>
2018-04-09 23:32:35 +0200

    add add button on homescreen

--- views/HomeScreen.js
+++ views/HomeScreen.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
+import { FloatingAction } from 'react-native-floating-action';
+
+import { palette } from '../styles';
 
 const styles = StyleSheet.create({
     container: {
@@ -19,12 +22,32 @@ export default class HomeScreen extends Component {
         title: 'Home',
     };
 
+    handleFloatingActionPressed() {
+        console.log('pressed');
+    }
+
     render() {
+        const floatingActions = [{
+            name: 'newItem',
+            icon: <Image source={require('../icons/camera_alt.png')} style={{
+                height: 32,
+                width: 32,
+            }} />,
+        }];
+
         return (
             <View style={styles.container}>
                 <Text>Open up App.js to start working on your app!</Text>
                 <Text>Changes you make will automatically reload.</Text>
                 <Text>Shake your phone to open the developer menu.</Text>
+
+                <FloatingAction
+                    actions={floatingActions}
+                    overrideWithAction={true}
+                    color={palette.primary}
+                    position="center"
+                    onPressItem={() => this.handleFloatingActionPressed()}
+                />
             </View>
         );
     }
